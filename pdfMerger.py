569a0646659886d3d772a7e26ec310b51b249e18
sairamishetty <sram0056@student.monash.edu>
2023-11-06 17:43:07 +1100

    Remove other files from main

--- pdfMerger.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-import sys
-import os
-merger = PyPDF2.PdfMerger()
-for file in os.listdir(os.curdir):
-    if file.endswith(".pdf"):
-    
-        merger.append(file)
-    merger.write("combinedDoc.pdf")
